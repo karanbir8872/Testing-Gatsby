44f28e6a480994aa7f796fd5faf4040d7b337524
karanbir8872 <wwwkaranbir786@gmail.com>
2020-03-13 23:04:24 +0530

    add page.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,10 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
-// You can delete this file if you're not using it
+exports.createPages = ({ actions: { createPage } }) => {
+  createPage({
+    path: "/with-context/",
+    component: require.resolve("./src/templates/with-context.js"),
+    context: {
+      title: "We Don’t Need No Stinkin’ GraphQL!",
+      content: "<p>This is page content.</p><p>No GraphQL required!</p>",
+    },
+  })
+}
